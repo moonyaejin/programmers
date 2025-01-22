c0ebdc1b8a3423e588780fe25ad0e593238db860
예진 <i0209i80@gmail.com>
2025-01-22 23:10:49 +0900

    [level 1] Title: 시저 암호, Time: 0.15 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    
+    for (char c : s) {
+        if(isupper(c)) {
+            answer += 'A' + (c - 'A' + n) % 26;
+        } else if (islower(c)) {
+            answer += 'a' + (c - 'a' + n) % 26;
+        } else {
+            answer += c;
+        }
+    }
+    return answer;
+}
